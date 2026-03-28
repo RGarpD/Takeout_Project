3ca8df1065481d4a695bed842ac078e05c5277ba
RGarpD <159305785+RGarpD@users.noreply.github.com>
2026-03-28 16:37:33 +0100

    Update ShoppingBag.java

--- src/ShoppingBag.java
+++ src/ShoppingBag.java
@@ -20,4 +20,20 @@ public class ShoppingBag<T extends PricedItem<Integer>>{
         }
         return totalPrice;
     }
+
+    public String getTotalItem(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Your ordered: \n");
+        for(Map.Entry<T, Integer> item : shoppingBag.entrySet()){
+            Food foodItem = (Food) item.getKey();
+            int quantity = item.getValue();
+            String foodName =  foodItem.getName();
+
+              sb.append(quantity)
+                .append("x ")
+                .append(foodName)
+                .append("\n");
+        }
+        return sb.toString();
+    }
 }
